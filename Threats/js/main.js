7210d30e2d5fdceb2f7ecbcb624952d367fe4447
Tommy Dang <nhontuan@gmail.com>
2019-05-14 10:54:43 -0500

    update arcs

--- Threats/js/main.js
+++ Threats/js/main.js
@@ -2,7 +2,7 @@ const margin = {left: 0, top: 20, right: 120, bottom: 20},
     networkWidth = 500,
     networkHeight = 500,
     timeArcWidth = window.innerWidth - networkWidth - margin.left - margin.right,
-    timeArcHeight = window.innerHeight - margin.top - margin.bottom - 100,
+    timeArcHeight = window.innerHeight - margin.top - margin.bottom - 220,
     svgWidth = networkWidth + timeArcWidth + margin.left + margin.right,
     svgHeight = Math.max(networkHeight, timeArcHeight) + margin.top + margin.bottom;
 
@@ -42,7 +42,7 @@ d3.csv('data/104.12.0.0.csv').then(data => {
     //links and nodes with combinations
     let targetsOfUnknownOnly = getTargetsOfUnknownOnly(data);
     //Remove (104.12.90.1)
-    targetsOfUnknownOnly = targetsOfUnknownOnly.filter(d => d != '104.12.90.1');
+    //targetsOfUnknownOnly = targetsOfUnknownOnly.filter(d => d != '104.12.90.1');
 
     let combinedNode = {id: 'combined', nodes: []};
     let tgoNodes = [];
