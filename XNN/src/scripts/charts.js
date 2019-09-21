f80ac361212a2a94c2912ab92f801bcfc0ad5b49
Pham Van Vung <phamvanvung@gmail.com>
2019-09-21 13:55:51 -0500

    Updated

--- XNN/src/scripts/charts.js
+++
@@ -1,8 +0,0 @@
-import {HeatMap} from "./modules/heatmap";
-import {LineChart} from "./modules/linechart";
-import {RadarChart} from "./modules/radarchart";
-
-window.HeatMap = HeatMap;
-window.LineChart = LineChart;
-window.RadarChart = RadarChart;
-
