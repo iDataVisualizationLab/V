a9adf09fc9fdaf9d757bde304133506bdc8bb60e
Pham Van Vung <phamvanvung@gmail.com>
2019-09-21 13:57:52 -0500

    Updated

--- XNN/js/index.js
+++ XNN/js/index.js
@@ -115,16 +115,16 @@ function processData(X_trainR, y_trainR, X_testR, y_testR, resolve) {
 
 async function processInputs() {
     return new Promise(resolve => {
-        d3.json("data/train_FD001_100x50.json").then(X_trainR => {
-            d3.json("data/train_RUL_FD001_100x50.json").then(y_trainR => {
-                d3.json("data/test_FD001_100x50.json").then(X_testR => {
-                    d3.json("data/test_RUL_FD001_100x50.json").then(y_testR => {
-                        features = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 20, 21].map(ss => "sensor" + ss);
-                        // d3.json("data/X_train_HPCC_1_20.json").then(X_trainR => {
-                        //     d3.json("data/y_train_HPCC_1_20.json").then(y_trainR => {
-                        //         d3.json("data/X_test_HPCC_1_20.json").then(X_testR => {
-                        //             d3.json("data/y_test_HPCC_1_20.json").then(y_testR => {
-                        //                 features = ['arrTemperature0', 'arrTemperature1', 'arrTemperature2', 'arrCPU_load0', 'arrMemory_usage0', 'arrFans_health0', 'arrFans_health1', 'arrFans_health2', 'arrFans_health3', 'arrPower_usage0'];
+        // d3.json("data/train_FD001_100x50.json").then(X_trainR => {
+        //     d3.json("data/train_RUL_FD001_100x50.json").then(y_trainR => {
+        //         d3.json("data/test_FD001_100x50.json").then(X_testR => {
+        //             d3.json("data/test_RUL_FD001_100x50.json").then(y_testR => {
+        //                 features = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 20, 21].map(ss => "sensor" + ss);
+        d3.json("data/X_train_HPCC_1_20.json").then(X_trainR => {
+            d3.json("data/y_train_HPCC_1_20.json").then(y_trainR => {
+                d3.json("data/X_test_HPCC_1_20.json").then(X_testR => {
+                    d3.json("data/y_test_HPCC_1_20.json").then(y_testR => {
+                        features = ['arrTemperature0', 'arrTemperature1', 'arrTemperature2', 'arrCPU_load0', 'arrMemory_usage0', 'arrFans_health0', 'arrFans_health1', 'arrFans_health2', 'arrFans_health3', 'arrPower_usage0'];
                         populateFeatureSelection(features);
                         selectedFeatures = features.map(_ => true);
                         X_train = copyFeatures(X_trainR, selectedFeatures);
