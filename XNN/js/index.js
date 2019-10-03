b3114000be81787765f30cc02b5663e844012d22
Pham Van Vung <phamvanvung@gmail.com>
2019-10-03 17:08:30 -0500

    Filter also the input nodes out.

--- XNN/js/index.js
+++ XNN/js/index.js
@@ -125,19 +125,19 @@ function processData(X_trainR, y_trainR, X_testR, y_testR, resolve) {
 //TODO: Since we load the default models => this might not be needed => but there are few dataset specific information that we need to save/load before by passing this. So check again.
 async function processInputs() {
     return new Promise(resolve => {
-        d3.json("data/train_FD001_100x50.json").then(X_trainR => {
-            d3.json("data/train_RUL_FD001_100x50.json").then(y_trainR => {
-                d3.json("data/test_FD001_100x50.json").then(X_testR => {
-                    d3.json("data/test_RUL_FD001_100x50.json").then(y_testR => {
-                        features = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 20, 21].map(ss => "sensor" + ss);
-                        predictedVariable = "RUL";
-                        dataItemName = "Engines";
-        // d3.json("data/X_train_HPCC_1_20.json").then(X_trainR => {
-        //     d3.json("data/y_train_HPCC_1_20.json").then(y_trainR => {
-        //         d3.json("data/X_test_HPCC_1_20.json").then(X_testR => {
-        //             d3.json("data/y_test_HPCC_1_20.json").then(y_testR => {
-        //                 features = ['arrTemperature0', 'arrTemperature1', 'arrTemperature2', 'arrCPU_load0', 'arrMemory_usage0', 'arrFans_health0', 'arrFans_health1', 'arrFans_health2', 'arrFans_health3', 'arrPower_usage0'];
-        //                 predictedVariable = "arrTemperature0";
+        // d3.json("data/train_FD001_100x50.json").then(X_trainR => {
+        //     d3.json("data/train_RUL_FD001_100x50.json").then(y_trainR => {
+        //         d3.json("data/test_FD001_100x50.json").then(X_testR => {
+        //             d3.json("data/test_RUL_FD001_100x50.json").then(y_testR => {
+        //                 features = [2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 15, 17, 20, 21].map(ss => "sensor" + ss);
+        //                 predictedVariable = "RUL";
+        //                 dataItemName = "Engines";
+        d3.json("data/X_train_HPCC_1_20.json").then(X_trainR => {
+            d3.json("data/y_train_HPCC_1_20.json").then(y_trainR => {
+                d3.json("data/X_test_HPCC_1_20.json").then(X_testR => {
+                    d3.json("data/y_test_HPCC_1_20.json").then(y_testR => {
+                        features = ['arrTemperature0', 'arrTemperature1', 'arrTemperature2', 'arrCPU_load0', 'arrMemory_usage0', 'arrFans_health0', 'arrFans_health1', 'arrFans_health2', 'arrFans_health3', 'arrPower_usage0'];
+                        predictedVariable = "arrTemperature0";
                         dataItemName = "Computes";
                         populateFeatureSelection(features);
                         selectedFeatures = features.map(_ => true);
